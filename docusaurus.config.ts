a1a1939fe0815d251317bab56202e575984c2ae2
roc <roc@imroc.cc>
2025-11-06 17:41:15 +0800

    update at 2025-11-06 17:41:15

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,6 @@
 import PrismDark from './src/utils/prismDark';
 import type { Config } from '@docusaurus/types';
-// import { themes as prismThemes } from 'prism-react-renderer';
+import type * as Preset from '@docusaurus/preset-classic';
 
 const beian = '蜀ICP备2021009081号-1'
 
@@ -9,6 +9,10 @@ const config: Config = {
   tagline: '云原生老司机带你飞', // slogan
   favicon: 'img/logo.svg', // 电子书 favicon 文件，注意替换
 
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
   url: 'https://imroc.cc', // 在线电子书的 url
   baseUrl: '/kubernetes/', // 在线电子书所在 url 的路径，如果没有子路径，可改为 "/"
 
@@ -51,162 +55,152 @@ const config: Config = {
         ],
       },
     ],
-    [
-      /** @type {import('@docusaurus/plugin-content-docs').PluginOptions} */
-      '@docusaurus/plugin-content-docs',
-      ({
-        id: 'kubernetes',
-        path: 'content',
-        showLastUpdateTime: true,
-        // 文档的路由前缀
-        routeBasePath: '/',
-        // 左侧导航栏的配置
-        sidebarPath: require.resolve('./content/sidebars.ts'),
-        // 每个文档左下角 "编辑此页" 的链接
-        editUrl: ({ docPath }) =>
-          `https://github.com/imroc/kubernetes-guide/edit/main/content/${docPath}`,
-      }),
-    ]
   ],
 
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: false, // 禁用 preset 默认的 docs，直接用 plugin-content-docs 配置可以更灵活。
+      {
+        docs: {
+          showLastUpdateTime: true,
+          // 文档的路由前缀
+          routeBasePath: '/',
+          // 左侧导航栏的配置
+          sidebarPath: require.resolve('./sidebars.ts'),
+          // 每个文档左下角 "编辑此页" 的链接
+          editUrl: ({ docPath }) =>
+            `https://github.com/imroc/kubernetes-guide/edit/main/docs/${docPath}`,
+        },
         blog: false, // 禁用博客
         theme: {
           customCss: require.resolve('./src/css/custom.scss'), // custom.css 重命名为 custom.scss
         },
-      }),
+      } satisfies Preset.Options,
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // algolia 搜索功能
-      algolia: {
-        appId: '7WGNV6HFP7',
-        apiKey: 'f3bd4ef4fa5dbfa7494fc901311e584d',
-        indexName: 'imroc-kubernetes',
-        contextualSearch: false,
+  themeConfig: {
+    // algolia 搜索功能
+    algolia: {
+      appId: '7WGNV6HFP7',
+      apiKey: 'f3bd4ef4fa5dbfa7494fc901311e584d',
+      indexName: 'imroc-kubernetes',
+      contextualSearch: false,
+    },
+    // giscus 评论功能
+    giscus: {
+      repo: 'imroc/kubernetes-guide',
+      repoId: 'R_kgDOG-4vhA',
+      category: 'General',
+      categoryId: 'DIC_kwDOG-4vhM4COPpN',
+    },
+    navbar: {
+      title: 'Kuberntes 实践指南', // 左上角的电子书名称
+      logo: {
+        alt: 'Kubernetes',
+        src: 'img/logo.svg', // 电子书 logo 文件，注意替换
       },
-      // giscus 评论功能
-      giscus: {
-        repo: 'imroc/kubernetes-guide',
-        repoId: 'R_kgDOG-4vhA',
-        category: 'General',
-        categoryId: 'DIC_kwDOG-4vhM4COPpN',
-      },
-      navbar: {
-        title: 'Kuberntes 实践指南', // 左上角的电子书名称
-        logo: {
-          alt: 'Kubernetes',
-          src: 'img/logo.svg', // 电子书 logo 文件，注意替换
+      items: [
+        {
+          label: 'Kubernetes 排障指南',
+          position: 'right',
+          href: 'https://imroc.cc/kubernetes-troubleshooting',
         },
-        items: [
-          {
-            label: 'Kubernetes 排障指南',
-            position: 'right',
-            href: 'https://imroc.cc/kubernetes-troubleshooting',
-          },
-          {
-            label: 'TKE 实践指南',
-            position: 'right',
-            href: 'https://imroc.cc/tke',
-          },
-          {
-            label: 'istio 实践指南',
-            position: 'right',
-            href: 'https://imroc.cc/istio',
-          },
-          {
-            href: 'https://github.com/imroc/kubernetes-guide', // 改成自己的仓库地址
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      // 自定义页脚
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: '相关电子书',
-            items: [
-              {
-                label: 'Kubernetes 排障指南',
-                href: 'https://imroc.cc/kubernetes-troubleshooting',
-              },
-              {
-                label: 'istio 实践指南',
-                href: 'https://imroc.cc/istio',
-              },
-              {
-                label: 'TKE 实践指南',
-                href: 'https://imroc.cc/tke',
-              },
-            ],
-          },
-          {
-            title: '更多',
-            items: [
-              {
-                label: 'roc 云原生',
-                href: 'https://imroc.cc',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/imroc/kubernetes-guide',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright ${new Date().getFullYear()} roc | All Right Reserved | <a href="http://beian.miit.gov.cn/">${beian}</a>`,
-      },
-      // 自定义代码高亮
-      prism: {
-        theme: PrismDark,
-        magicComments: [
-          {
-            className: 'code-block-highlighted-line',
-            line: 'highlight-next-line',
-            block: { start: 'highlight-start', end: 'highlight-end' }
-          },
-          {
-            className: 'code-block-add-line',
-            line: 'highlight-add-line',
-            block: { start: 'highlight-add-start', end: 'highlight-add-end' }
-          },
-          {
-            className: 'code-block-update-line',
-            line: 'highlight-update-line',
-            block: { start: 'highlight-update-start', end: 'highlight-update-end' }
-          },
-          {
-            className: 'code-block-error-line',
-            line: 'highlight-error-line',
-            block: { start: 'highlight-error-start', end: 'highlight-error-end' }
-          },
-        ],
-        // languages enabled by default: https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L23
-        // prism supported languages: https://prismjs.com/#supported-languages
-        additionalLanguages: [
-          'java',
-          'json',
-          'hcl',
-          'bash',
-          'diff',
-          'docker',
-          'nginx',
-          'toml',
-          'systemd',
-          'nginx',
-        ],
-      },
-    }),
+        {
+          label: 'TKE 实践指南',
+          position: 'right',
+          href: 'https://imroc.cc/tke',
+        },
+        {
+          label: 'istio 实践指南',
+          position: 'right',
+          href: 'https://imroc.cc/istio',
+        },
+        {
+          href: 'https://github.com/imroc/kubernetes-guide', // 改成自己的仓库地址
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    // 自定义页脚
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: '相关电子书',
+          items: [
+            {
+              label: 'Kubernetes 排障指南',
+              href: 'https://imroc.cc/kubernetes-troubleshooting',
+            },
+            {
+              label: 'istio 实践指南',
+              href: 'https://imroc.cc/istio',
+            },
+            {
+              label: 'TKE 实践指南',
+              href: 'https://imroc.cc/tke',
+            },
+          ],
+        },
+        {
+          title: '更多',
+          items: [
+            {
+              label: 'roc 云原生',
+              href: 'https://imroc.cc',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/imroc/kubernetes-guide',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright ${new Date().getFullYear()} roc | All Right Reserved | <a href="http://beian.miit.gov.cn/">${beian}</a>`,
+    },
+    // 自定义代码高亮
+    prism: {
+      theme: PrismDark,
+      magicComments: [
+        {
+          className: 'code-block-highlighted-line',
+          line: 'highlight-next-line',
+          block: { start: 'highlight-start', end: 'highlight-end' }
+        },
+        {
+          className: 'code-block-add-line',
+          line: 'highlight-add-line',
+          block: { start: 'highlight-add-start', end: 'highlight-add-end' }
+        },
+        {
+          className: 'code-block-update-line',
+          line: 'highlight-update-line',
+          block: { start: 'highlight-update-start', end: 'highlight-update-end' }
+        },
+        {
+          className: 'code-block-error-line',
+          line: 'highlight-error-line',
+          block: { start: 'highlight-error-start', end: 'highlight-error-end' }
+        },
+      ],
+      // languages enabled by default: https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L23
+      // prism supported languages: https://prismjs.com/#supported-languages
+      additionalLanguages: [
+        'java',
+        'json',
+        'hcl',
+        'bash',
+        'diff',
+        'docker',
+        'nginx',
+        'toml',
+        'systemd',
+        'nginx',
+      ],
+    },
+  } satisfies Preset.ThemeConfig,
   scripts: [
     {
       src: "https://imroc.cc/kubernetes/js/BigPicture.min.js",
